46907905024dcb1703f875803e95d17b6352e9e3
sakshid23 <sakshi2018d@gmail.com>
2025-07-12 17:47:06 +0530

    Add Maven Java project

---
+++ src/test/java/com/sakshi/demo/AppTest.java
@@ -0,0 +1,12 @@
+package com.sakshi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
